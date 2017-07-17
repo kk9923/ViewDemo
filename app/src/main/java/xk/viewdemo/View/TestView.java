65a2c6c0dbe324326aac379953d7c260b86a1ab0
renwoxing <renwoxing@yuruiqi.com>
2017-07-17 14:57:02 +0800

    Initial commit

---
+++ app/src/main/java/xk/viewdemo/View/TestView.java
@@ -0,0 +1,25 @@
+package xk.viewdemo.View;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by 31716 on 2017/7/17.
+ */
+
+public class TestView extends View {
+    public TestView(Context context) {
+        super(context);
+
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
